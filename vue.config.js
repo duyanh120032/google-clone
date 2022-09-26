50585b88a2a8e10daa8bb57e51f822063c0043c3
*lyZCWKmo9M <ronny.duytran@gumiviet.com>
2022-09-26 10:39:35 +0700

    fix : change path directions

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,12 @@ module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
-      require('unplugin-vue-components/webpack')({ /* options */ }),
+      require('unplugin-vue-components/webpack')({
+        dts: 'src/components.d.ts',
+      }),
       require('unplugin-auto-import/webpack')({
         imports: ['vue', 'vue-router', '@vueuse/core'],
+        dts: 'src/auto-imports.d.ts',
       }),
     ],
   },
