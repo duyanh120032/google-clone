9e6345ac1cbccffb0566eb85f02caf85786e0d2b
*lyZCWKmo9M <ronny.duytran@gumiviet.com>
2022-09-26 14:22:12 +0700

    feat : setup seo

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,7 +7,6 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Results: typeof import('./components/Results/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
