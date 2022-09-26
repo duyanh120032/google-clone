f24458c54d059da3bd76af0c9ca8ac19891bb609
*lyZCWKmo9M <ronny.duytran@gumiviet.com>
2022-09-26 10:10:38 +0700

    chore : setup unplugins

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      require('unplugin-vue-components/webpack')({ /* options */ }),
+      require('unplugin-auto-import/webpack')({
+        imports: ['vue', 'vue-router', '@vueuse/core'],
+      }),
+    ],
+  },
 })
